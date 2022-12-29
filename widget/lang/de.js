ae652654c93e89a17e8c787669a265c57d23e9ff
LochanLoch <66974900+LochanLoch@users.noreply.github.com>
2022-12-29 17:29:29 +0100

    Create de.js
    for German

---
+++ widget/lang/de.js
@@ -0,0 +1,20 @@
+export default {
+  powered_by: 'Kommentare verwaltet von Cusdis',
+  post_comment: 'Kommentar',
+  loading: 'Es wird geladen',
+
+  // comment
+  email: 'E-Mail (optional)',
+  nickname: 'Benutzername',
+  reply_placeholder: 'Erwiderung ...',
+  reply_btn: 'Antwort',
+  sending: 'Einsenden ...',
+
+  // reply
+  mod_badge: 'Moderator',
+  content_is_required: 'Inhalt ist erforderlich',
+  nickname_is_required: 'Ein Benutzername ist erforderlich',
+
+  comment_has_been_sent:
+    'Dein Kommentar wurde versendet. Bitte warten Sie auf die Genehmigung.',
+}
